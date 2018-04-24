d1ab64e4d3111de62e9406f5d7580a53038690ba
Wahyu <adjieb.wahyu@gmail.com>
2018-04-24 18:34:31 +0800

    Mengisi Nilai Attribut

--- src/Praktik02/RectangleAksi.java
+++ src/Praktik02/RectangleAksi.java
@@ -7,5 +7,7 @@ package Praktik02;
 public class RectangleAksi {
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle();
+        r1.panjang = 9;
+        r1.lebar    = 3;
     }
 }
