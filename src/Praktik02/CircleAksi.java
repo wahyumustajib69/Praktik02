f1460cc8f0ecc3072c8268d7b99e927b7e3aea66
Wahyu <adjieb.wahyu@gmail.com>
2018-05-02 21:54:53 +0800

    Memanggil Method / Behavior

--- src/Praktik02/CircleAksi.java
+++ src/Praktik02/CircleAksi.java
@@ -1,6 +1,10 @@
 package Praktik02;
 public class CircleAksi {
     public static void main(String[] args) {
-        Circle c1= new Circle();
+        Circle c1 = new Circle();
+        c1.phi = 3.14;
+        c1.jari = 9;
+        
+        c1.cetakinfo();
     }
 }
