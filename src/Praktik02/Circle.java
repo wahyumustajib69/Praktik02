46425a69fd454fc61d7ec1c7053251d5d23304bb
Wahyu <adjieb.wahyu@gmail.com>
2018-05-02 21:50:39 +0800

    Membuat Attribut dan Behavior

--- src/Praktik02/Circle.java
+++ src/Praktik02/Circle.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Praktik02;
-
-/**
- *
- * @author WAHYU'S PC
- */
 public class Circle {
+    double phi,jari;
     
+    void cetakinfo(){
+        System.out.println("=======================");
+        System.out.println("PHI         : "+phi);
+        System.out.println("JARI-JARI   : "+jari);
+        System.out.println("=======================");
+    }
 }
