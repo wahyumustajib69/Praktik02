627a660e2019214f7358ae7a50cc4faca082af0b
Wahyu <adjieb.wahyu@gmail.com>
2018-05-02 21:43:31 +0800

    Membuat Class Circle

---
+++ src/Praktik02/Circle.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktik02;
+
+/**
+ *
+ * @author WAHYU'S PC
+ */
+public class Circle {
+    
+}
